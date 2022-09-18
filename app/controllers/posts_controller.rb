94abc451012e85e327eafb7a13e283ee8b8e954e
ichigoflorentin <r1ka15@yahoo.co.jp>
2022-09-18 12:41:28 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   # end
 
   def create
-    post = ost.create(content: params[:content])
-    render json{post: post}
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
